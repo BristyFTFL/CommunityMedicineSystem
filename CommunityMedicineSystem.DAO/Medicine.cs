7c3dc802b9c9230ba8460b0659141dbd15816852
tayabur-masud <msd.unknown@gmail.com>
2015-02-01 16:18:58 +0600

    Community Medicine System Version 1 Startup

---
+++ CommunityMedicineSystem.DAO/Medicine.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunityMedicineSystem.DAO
+{
+    public class Medicine
+    {
+        public int Id { set; get; }
+        public string Name { set; get; }
+        public decimal Power { set; get; }
+        public string Type { set; get; }
+        public string NamePowerType { set; get; }
+    }
+}
